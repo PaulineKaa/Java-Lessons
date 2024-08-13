a19dc6dc085b08591721cf9e2440e4e6409a1965
Pauline <53222278+PaulineKaa@users.noreply.github.com>
2024-08-13 18:03:51 +0300

    Update ru.job4j.calculator.Calculator.java

--- ru.job4j.calculator.Calculator.java
+++ ru.job4j.calculator.Calculator.java
@@ -1,14 +1,14 @@
 void main() {
-	  int one = 1;
-    int two = 2;
+	int one = 1;
+	int two = 2;
   	int four = 4;
   	int five = 5;
   	int six = 6;
-    int onePlusTwo = one + two;
+	int onePlusTwo = one + two;
   	int sixDivTwo = six/two;
   	int fiveMinusTwo = five - two;
   	int fourTimeTwo = four * two;
-    System.out.println(onePlusTwo);
+	System.out.println(onePlusTwo);
   	System.out.println(sixDivTwo);
   	System.out.println(fiveMinusTwo);
   	System.out.println(fourTimeTwo);
